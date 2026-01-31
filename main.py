d492d09d8df5f4a591b677ee7baa63ab492ad399
jonathankabangu1-max <jonathankabangu1@gmail.com>
2026-01-31 11:08:41 +0100

    Update main.py
    Je suis Jonathan KABANGU "Jokab " apprenant cyberdev, ma contribution dans le code de nombre aléatoire

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # Voici notre premier project
 # nous voulons un code python qui permet a l'utilisateur de deviner un nombre:
 # Entrer un nombre, et quand l'utilisateur entre le nombre, ça dit si le nombre entré est grand ou petit. ou il a trouver le bon nombre
+
+
+
+import randon  # fonction de nombre aléatoire 
+A= random.randint(0, 20) # nombre aléatoire comprise entre 0 et 20
+N= int(input()) # Entrer un nombre entier 
+if N>A:
+  print(N " est plus grand que le nombre aléatoire ")
+else if N<A:
+  print(N " est plus que le nombre aléatoire ")
+else 
+  print(A "Nombre aléatoire ")
+  print(N " nombre saisie correspondant au nombre aléatoire ")
