e9155139fd15e5a5c6522d3489a252a7944383ee
Nesi05 <46183636+Nesi05@users.noreply.github.com>
2026-02-06 23:48:07 -0800

    Delete main.py

--- main.py
+++
@@ -1,4 +0,0 @@
-# Voici notre premier project
-# nous voulons un code python qui permet a l'utilisateur de deviner un nombre:
-# Entrer un nombre, et quand l'utilisateur entre le nombre, ça dit si le nombre entré est grand ou petit. ou il a trouver le bon nombre
-print("Bienvenue chez CybeDev")
